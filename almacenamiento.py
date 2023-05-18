bdbb4656865e89e58546e3aad6c94ccc8bc68ede
rubenexpensive <rdariocarolopez@gmail.com>
2023-05-18 17:16:56 -0500

    Hola

--- almacenamiento.py
+++ almacenamiento.py
@@ -1,4 +1,11 @@
-import mysql
+import mysql.connector
 
+conexio = mysql.connector.connect(user = 'root', password = '',host = '127.0.0.1')
+cursor = conexio.cursor()
+sql = "CREATE DATABASE IF NOR EXISTS TIENDA3"
+cursor.execute(sql)
+conexio.commit()
+cursor.close()
+conexio.close()
 
 
